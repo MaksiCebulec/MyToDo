649e908300e775496604c45ae6ea09dca09e3403
Maksi <maksi.cebulec@gmail.com>
2022-09-06 17:56:24 +0200

    Third commit Added and tested mongoose

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017/my-todo')
+    .then(() => {
+        console.log('Success');
+    })
+    .catch((err) => {
+        console.log('Error');
+        console.log(err);
+    })
+
+
+const TasksSchema = new mongoose.Schema({
+    title: String,
+    description: String
+});
+
+const Task = mongoose.model('Task', TasksSchema);
+
+
+
+
 
 app.set('view engine', 'views');
 app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/mytodo', (req, res) => {
+app.get('/mytodo', async (req, res) => {
+    const newTask = new Task({ title: 'Clean the room', description: 'I have to clean the room today!!!' });
+    await newTask.save();
+    console.log(newTask);
     res.render('index.ejs');
 })
 
