da04ef719adb5492436f791862c201d6cad46f37
Maksi <maksi.cebulec@gmail.com>
2022-09-17 13:15:09 +0200

    11th commit added Error handling, added required title to tasks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const AppError = require('./utils/AppError');
+const catchAsync = require('./utils/catchAsync');
 const app = express();
 
 
@@ -29,44 +31,71 @@ app.use(express.urlencoded());
 app.use(methodOverride('_method'));
 
 
-app.get('/mytodo', async (req, res) => {
-    const tasks = await Task.find({});
-    res.render('index.ejs', { tasks });
+app.get('/mytodo', async (req, res, next) => {
+    try {
+        const tasks = await Task.find({});
+        res.render('index.ejs', { tasks });
+    } catch (e) {
+        next(e);
+    }
+
 });
 
 app.get('/mytodo/new', (req, res) => {
     res.render('new.ejs');
 });
 
-app.get('/mytodo/:id', async (req, res) => {
+app.get('/mytodo/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const task = await Task.findById(id);
+    if (!task) {
+        throw new AppError('No task with that ID', 404);
+    }
     res.render('show.ejs', { task });
-});
 
-app.get('/mytodo/:id/edit', async (req, res) => {
+}));
+
+app.get('/mytodo/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const task = await Task.findById(id);
+    if (!task) {
+        throw new AppError('Cant edit, No task with that ID', 404);
+    }
     res.render('edit.ejs', { task });
-});
 
-app.put('/mytodo/:id', async (req, res) => {
+}));
+
+app.put('/mytodo/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const task = await Task.findByIdAndUpdate(id, { ...req.body.task });
     res.redirect(`/mytodo/${id}`);
-})
 
-app.delete('/mytodo/:id', async (req, res) => {
+}));
+
+app.delete('/mytodo/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const task = await Task.findByIdAndDelete(id);
     res.redirect('/mytodo')
-});
 
-app.post('/mytodo', async (req, res) => {
+}));
+
+app.post('/mytodo', catchAsync(async (req, res) => {
     const newTask = Task({ ...req.body.task });
     await newTask.save();
     res.redirect(`/mytodo/${newTask._id}`);
-});
+
+}));
+
+
+app.all('*', (req, res, next) => {
+    console.log('ERR')
+    next(new AppError('Page not found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).send(message);
+})
 
 
 
---
+++ utils/AppError.js
@@ -0,0 +1,8 @@
+module.exports = class AppError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
